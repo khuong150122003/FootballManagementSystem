4539974c3cd1dfdad8d7395dfb077114eef520bb
Trần Nhật Khương <110782685+khuong150122003@users.noreply.github.com>
2023-05-11 09:37:48 +0700

    Update Program.cs

--- FootballManagementSystem/Program.cs
+++ FootballManagementSystem/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,14 +9,15 @@ namespace FootballManagementSystem
    
         class Program
         {
-            static void Menu()
+        private static bool found;
+
+        static void Menu()
             {
                 Console.WriteLine("1. Create new Player");
-                Console.WriteLine("2. List of players");
-                Console.WriteLine("3. Search player by name");
-                Console.WriteLine("4. Raise attack event");
-                Console.WriteLine("5. Raise defense event");
-                Console.WriteLine("6. Exit");
+                Console.WriteLine("2. List of Players");
+                Console.WriteLine("3. Search Player by name");
+                Console.WriteLine("4. Delete Player");
+                Console.WriteLine("5. Exit");
             }
             static void Main(string[] args)
             {
@@ -40,10 +41,10 @@ namespace FootballManagementSystem
                             else
                             {
                                 foreach (IPlayer player1 in club)
-                                {
-                                    player1.GetInfo();
-                                }
+                            {
+                                player1.GetInfo();
                             }
+                        }
                             break;
                         case 3:
                             if (club.PlayerList.Count == 0)
@@ -69,13 +70,32 @@ namespace FootballManagementSystem
                                 }
                             }
                             break;
-                        case 4:
-                            club.RaiseAttackEvent();
-                            break;
-                        case 5:
-                            club.RaiseDefenseEvent();
-                            break;
-                        case 6: return;
+case 4:
+                        Console.Write("\nEnter player name to delete: ");
+                        string playername = Console.ReadLine();
+                        found = false;
+
+                        foreach (IPlayer player1 in club)
+                        {
+                            if (player1.Name == playername)
+                            {
+                                club.DeletePlayer(player1);
+                                found = true;
+                                break;
+                            }
+                        }
+
+                        if (found)
+                        {
+                            Console.WriteLine("\nPlayer deleted successfully.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nPlayer not found.");
+                        }
+
+                        break;
+                        case 5: return;
                     }
                 }
             }
