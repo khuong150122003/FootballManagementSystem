662cd48bea05f22f9dad876ecf136f51e07f3825
Trần Nhật Khương <110782685+khuong150122003@users.noreply.github.com>
2023-05-11 09:38:37 +0700

    Update IPlayer.cs

--- FootballManagementSystem/IPlayer.cs
+++ FootballManagementSystem/IPlayer.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,18 +6,53 @@ using System.Threading.Tasks;
 
 namespace FootballManagementSystem
 {
-   
-        interface IPlayer
-        {
-            string Name { get; set; }
-            int Age { get; set; }
-            int Attack { get; set; }
-            int Defense { get; set; }
-            int Stamina { get; set; }
-            int Speed { get; set; }
-            int Power { get; set; }
+    internal class IPlayer : Object
+    {
 
-            void GetInfo();
+        public string Name;
+        public int Age;
+        public int attack;
+        public int defense;
+        public int stamina;
+        public int speed;
+        public int power;
+
+        public IPlayer(string name, int age, int attack, int defense, int stamina, int speed, int power)
+        {
+            Name = name;
+            Age = age;
+            this.attack = attack;
+            this.defense = defense;
+            this.stamina = stamina;
+            this.speed = speed;
+            this.power = power;
         }
+
+
+        public string name { get { return Name; } set { Name = value; } }
+        public string age { get { return age; } set { age = value; } }
+        public int Attack { get { return attack; } set { attack = value; } }
+
+        public int Defense { get { return defense; } set { defense = value; } }
+
+        public int Stamina { get { return stamina; } set { stamina = value; } }
+
+        public int Speed { get { return speed; } set { speed = value; } }
+
+        public int Power { get { return power; } set { power = value; } }
+
+        public IPlayer() { }
+        public void GetInfo()
+        {
+            Console.WriteLine("Name: {0}", Name);
+            Console.WriteLine("Age: {0}", Age);
+            Console.WriteLine("Attack: {0}", Attack);
+            Console.WriteLine("Defense: {0}", Defense);
+            Console.WriteLine("Stamina: {0}", Stamina);
+            Console.WriteLine("Speed: {0}", Speed);
+            Console.WriteLine("Power: {0}", Power);
+        }
+
     }
+}
 
