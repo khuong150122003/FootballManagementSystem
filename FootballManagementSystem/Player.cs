89d4fdd7acae932197b9c8759531323c9c291ea3
Trần Nhật Khương <110782685+khuong150122003@users.noreply.github.com>
2023-05-11 09:38:15 +0700

    Update Player.cs

--- FootballManagementSystem/Player.cs
+++ FootballManagementSystem/Player.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,30 +6,10 @@ using System.Threading.Tasks;
 
 namespace FootballManagementSystem
 {
-    
-        class Player : IPlayer
-        {
-            string name;
-            public string Name { get { return name; } set { name = value; } }
-            int age;
-            public int Age { get { return age; } set { age = value; } }
-            int attack;
-            public int Attack { get { return attack; } set { attack = value; } }
-            int defense;
-            public int Defense { get { return defense; } set { defense = value; } }
-            int stamina;
-            public int Stamina { get { return stamina; } set { stamina = value; } }
-            int speed;
-            public int Speed { get { return speed; } set { speed = value; } }
-            int power;
-            public int Power { get { return power; } set { power = value; } }
 
-            public void GetInfo()
-            {
-                Console.WriteLine("Name: " + Name);
-                Console.WriteLine("Age: " + Age);
-                Console.WriteLine("Average point: " + (Attack + Defense + Stamina + Speed + Power) / 5.0f);
-            }
-        }
+    internal class Player : IPlayer
+    {
+   
     }
+}
 
